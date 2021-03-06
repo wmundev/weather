1bcfcaf31aa727174566fe2875b65feb5696e44d
Wilson Mun <30316250+wmundev@users.noreply.github.com>
2021-03-07 00:11:49 +1100

    add schedule for daily weather

--- weather-backend/Services/Scheduler/Scheduler.cs
+++ weather-backend/Services/Scheduler/Scheduler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using weather_backend.Models;
 
 namespace weather_backend.Services
 {
@@ -10,25 +12,33 @@ namespace weather_backend.Services
         private EmailService _emailService;
         private CurrentWeatherData _currentWeatherData;
         private ILogger<Scheduler> _logger;
+        private IConfiguration _configuration;
 
-        public Scheduler(EmailService emailService, CurrentWeatherData currentWeatherData, ILogger<Scheduler> logger) :
+        public Scheduler(EmailService emailService, CurrentWeatherData currentWeatherData, ILogger<Scheduler> logger,
+            IConfiguration configuration) :
             base("0 22 * * *", TimeZoneInfo.Utc)
         {
             _emailService = emailService;
             _currentWeatherData = currentWeatherData;
             _logger = logger;
+            _configuration = configuration;
         }
 
-        public override Task DoWork(CancellationToken cancellationToken)
+        public override async Task DoWork(CancellationToken cancellationToken)
         {
-            _logger.Log(LogLevel.Information, "wow crazy man");
-            return Task.CompletedTask;
+            //melbourne cityid: 7839805
+            double cityId = 7839805;
+            WeatherData weatherData = await _currentWeatherData.GetCurrentWeatherDataByCityId(cityId);
+
+            _emailService.SendEmail($"{weatherData.name} Current Weather",
+                $"Current Temperature: {weatherData.main.temp}, Humidity: {weatherData.main.humidity}",
+                _configuration.GetValue<string>("SMTPUsername"));
+            // return Task.CompletedTask;
         }
 
         public override Task StopAsync(CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
-
     }
 }

953ddd56411f6812562af6ed0dde56f2f6f50540
Wilson Mun <30316250+wmundev@users.noreply.github.com>
2021-03-18 01:10:37 +1100

    fix creds

--- ci-scripts/setup_env.sh
+++ ci-scripts/setup_env.sh
@@ -5,6 +5,6 @@ sed -i "s/{{DB_PASSWORD}}/${DB_PASSWORD}/g" weather-backend/appsettings.json
 sed -i "s/{{DB_USER}}/${DB_USER}/g" weather-backend/appsettings.json
 sed -i "s/{{DB_DATABASE}}/${DB_DATABASE}/g" weather-backend/appsettings.json
 # for docker compose config
-echo $DB_PASSWORD >> weather-backend/POSTGRES_PASSWORD.txt
-echo $DB_USER >> weather-backend/POSTGRES_USER.txt
-echo $DB_DATABASE >> weather-backend/POSTGRES_DB.txt
+echo $DB_PASSWORD >> POSTGRES_PASSWORD.txt
+echo $DB_USER >> POSTGRES_USER.txt
+echo $DB_DATABASE >> POSTGRES_DB.txt

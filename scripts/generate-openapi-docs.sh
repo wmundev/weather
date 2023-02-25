c40d727a60571c65590e3228f1adc687a7eb6285
Wilson Mun <30316250+wmundev@users.noreply.github.com>
2023-02-25 15:27:46 +1100

    Add generate swagger docs on commit

---
+++ scripts/generate-openapi-docs.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+(
+  cd ../
+  # Install swagger tool  
+  dotnet tool install -g --version 6.5.0 Swashbuckle.AspNetCore.Cli
+    
+  # Build app for release
+  dotnet publish -c Release -o out weather-backend/weather-backend.csproj
+  
+  swagger tofile --output api.yaml --yaml out/weather-backend.dll v1
+)

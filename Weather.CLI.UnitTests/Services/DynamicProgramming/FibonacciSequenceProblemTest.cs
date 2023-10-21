57f1847a2cbdc6681e7275bac5fbcc0787ca9c8d
Wilson Mun <30316250+wmundev@users.noreply.github.com>
2023-10-21 15:43:17 +1100

    feat: support long numbers in fibonacci sequence

--- Weather.CLI.UnitTests/Services/DynamicProgramming/FibonacciSequenceProblemTest.cs
+++ Weather.CLI.UnitTests/Services/DynamicProgramming/FibonacciSequenceProblemTest.cs
@@ -14,7 +14,8 @@ namespace Weather.CLI.UnitTests.Services.DynamicProgramming
         [InlineData(7, 13)]
         [InlineData(8, 21)]
         [InlineData(9, 34)]
-        private void SolveProblem(int problem, int solution)
+        [InlineData(50, 12586269025)]
+        private void SolveProblem(long problem, long solution)
         {
             // int[] problem = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             // int[] solution = new[] { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
--- Weather.CLI/Services/DynamicProgramming/FibonacciSequenceProblem.cs
+++ Weather.CLI/Services/DynamicProgramming/FibonacciSequenceProblem.cs
@@ -2,22 +2,21 @@ namespace Weather.CLI.Services.DynamicProgramming
 {
     public sealed class FibonacciSequenceProblem
     {
-        public static int SolveProblem(int haha)
+        public static long SolveProblem(long problemIndex)
         {
-            var problemIndex = haha;
-            int firstValue = 1;
-            int secondValue = 1;
-            if (problemIndex == 1 || problemIndex == 2)
+            const long firstValue = 1;
+            const long secondValue = 1;
+            if (problemIndex is 1 or 2)
             {
                 return firstValue;
             }
 
-            var memoMap = new Dictionary<int, int>();
+            var memoMap = new Dictionary<long, long>();
             memoMap.Add(1, firstValue);
             memoMap.Add(2, secondValue);
 
-            int solutionIndex = 2;
-            int result = 0;
+            long solutionIndex = 2;
+            long result = 0;
             do
             {
                 solutionIndex++;

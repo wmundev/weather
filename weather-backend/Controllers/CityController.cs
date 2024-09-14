7d1ccbd2c098049397ca22c9a9000c126b2320b2
Wilson Mun <30316250+wmundev@users.noreply.github.com>
2024-09-14 22:38:43 +1000

    feat: add in docs for city controller

--- weather-backend/Controllers/CityController.cs
+++ weather-backend/Controllers/CityController.cs
@@ -15,8 +15,18 @@ namespace weather_backend.Controllers
     {
         private readonly CityList _cityList = cityList ?? throw new ArgumentNullException(nameof(cityList));
 
+        /// <summary>
+        /// Retrieves a list of cities in Australia, limited by the specified number of cities.
+        /// </summary>
+        /// <param name="numberOfCities">The maximum number of cities to return. Defaults to 100. Must not exceed 500.</param>
+        /// <returns>
+        /// An <see cref="ActionResult"/> containing an <see cref="IEnumerable{City}"/> of cities in Australia.
+        /// Returns a <see cref="ProblemDetails"/> if the number of cities exceeds 500.
+        /// </returns>
         [HttpGet]
         [Route("all")]
+        [ProducesResponseType(400, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<City>))]
         public ActionResult<IEnumerable<City>> GetCities([FromQuery(Name = "num")] int numberOfCities = 100)
         {
             if (numberOfCities > 500)
@@ -34,6 +44,14 @@ namespace weather_backend.Controllers
         //     return Ok(1);
         // }
 
+        /// <summary>
+        /// Retrieves information about a specific city by its name.
+        /// </summary>
+        /// <param name="name">The name of the city to retrieve information for.</param>
+        /// <returns>
+        /// An <see cref="ActionResult"/> containing the <see cref="DynamoDbCity"/> information.
+        /// Returns a <see cref="NotFoundResult"/> if the city is not found.
+        /// </returns>
         [HttpGet]
         [Route("{name}")]
         public async Task<ActionResult<DynamoDbCity>> GetCityInformation(string name)
